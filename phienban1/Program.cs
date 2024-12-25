d8406cd6103cc38415478c2903cd381b42e88db6
phienban2 <trongtu0911@gmail.com>
2024-12-25 16:13:05 +0700

    https://github.com/TrongTu06/GIT_CODE.git

--- phienban1/Program.cs
+++ phienban1/Program.cs
@@ -11,6 +11,7 @@ namespace phienban1
         static void Main(string[] args)
         {
             Console.WriteLine("xin chao");
+            Console.WriteLine("hello bro");
             Console.ReadLine();
         }
     }
