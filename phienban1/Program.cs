5501872b57a6813bafb67b502e40941ad9499566
ADMIN <trongtu0911@gmail.com>
2024-12-25 16:06:55 +0700

    Add project files.

---
+++ phienban1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phienban1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("xin chao");
+            Console.ReadLine();
+        }
+    }
+}
